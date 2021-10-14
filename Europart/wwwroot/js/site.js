98d04685170a03097e7bb8ffbe49ae20a8098a18
Emiel Dehaen <emiel.dehaen@student.hogent.be>
2021-10-14 12:40:19 +0200

    Filter javascript

--- Europart/wwwroot/js/site.js
+++ Europart/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#filters").on("click", function (e) {
+        e.preventDefault();
+        let filter = document.getElementById("f-row");
+        if (filter.classList.contains("show")) {
+            $("#f-row").removeClass("show");
+        } else {
+            $("#f-row").addClass("show");
+            let elem = document.getElementsByClassName("col-4");
+            for (i = 0; i < elem.length; i++) {
+                console.log(elem[i]);
+                elem[i].classList.remove("col-4");
+                elem[i].classList.add("col-3");
+            }
+        }
+        
+    });
+})
