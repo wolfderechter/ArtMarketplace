fe3b328bb8021e43fc3b680a3a325fc936cd0004
Emiel Dehaen <emiel.dehaen@student.hogent.be>
2021-10-14 15:03:54 +0200

    Quick fixes

--- Europart/wwwroot/js/site.js
+++ Europart/wwwroot/js/site.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         let filter = document.getElementById("f-row");
         let elem = document.getElementsByClassName("kol");
         if (filter.classList.contains("show")) {
+            $(this).html("Toon filters");
             $("#f-row").removeClass("show");
             for (i = 0; i < elem.length; i++) {
                 elem[i].classList.remove("col-3");
@@ -16,6 +17,7 @@ $(document).ready(function () {
                 elem[i].classList.add("col-4");
             }
         } else {
+            $(this).html("Verberg filters");
             $("#f-row").addClass("show");
             for (i = 0; i < elem.length; i++) {
                 elem[i].classList.add("col-3");
