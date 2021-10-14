f0703e2e024c9e79e6529fcf9d28a851a7e31fe7
Emiel Dehaen <emiel.dehaen@student.hogent.be>
2021-10-14 12:51:47 +0200

    Update filters javascript

--- Europart/wwwroot/js/site.js
+++ Europart/wwwroot/js/site.js
@@ -6,15 +6,22 @@ $(document).ready(function () {
     $("#filters").on("click", function (e) {
         e.preventDefault();
         let filter = document.getElementById("f-row");
+        let elem = document.getElementsByClassName("kol");
         if (filter.classList.contains("show")) {
             $("#f-row").removeClass("show");
+            for (i = 0; i < elem.length; i++) {
+                elem[i].classList.remove("col-3");
+            }
+            for (i = 0; i < elem.length; i++) {
+                elem[i].classList.add("col-4");
+            }
         } else {
             $("#f-row").addClass("show");
-            let elem = document.getElementsByClassName("col-4");
             for (i = 0; i < elem.length; i++) {
-                console.log(elem[i]);
-                elem[i].classList.remove("col-4");
                 elem[i].classList.add("col-3");
+            }
+            for (i = 0; i < elem.length; i++) {
+                elem[i].classList.remove("col-4");
             }
         }
         
