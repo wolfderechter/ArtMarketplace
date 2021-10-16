3ba6c1a24fbdbf6258b9486e2e700a7ad27ab87d
zowieverschuere <60930219+zowieverschuere@users.noreply.github.com>
2021-10-16 14:34:11 +0200

    DCD begin

---
+++ Europart/Models/Domain/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Europart.Models.Domain
+{
+    public class Artist
+    {
+
+        public string Email { get; set; }
+    }
+}
