8622c0b00a0c60e57e3a9db96c46464b926a06c8
Emiel Dehaen <emiel.dehaen@student.hogent.be>
2021-10-26 13:10:46 +0200

    Linter fixes met vs coder formatter

--- Europart/Client/wwwroot/js/site.js
+++ Europart/Client/wwwroot/js/site.js
@@ -1,9 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-$(document).ready(function () {
-    $("#filters").on("click", function (e) {
+﻿$(document).ready(function() {
+    $("#filters").on("click", function(e) {
         e.preventDefault();
         let filter = document.getElementById("f-row");
         let elem = document.getElementsByClassName("kol");
