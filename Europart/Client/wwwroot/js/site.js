4c4369d5e7aea95cd83c53e83cfd0c89c2adf837
Emiel Dehaen <emiel.dehaen@student.hogent.be>
2021-10-26 14:14:32 +0200

    Linter formatting manual

--- Europart/Client/wwwroot/js/site.js
+++ Europart/Client/wwwroot/js/site.js
@@ -1,25 +1,27 @@
-﻿$(document).ready(function() {
-    $("#filters").on("click", function(e) {
+﻿/* global $ */
+
+$(document).ready(function () {
+    $('#filters').on('click', function (e) {
         e.preventDefault();
-        let filter = document.getElementById("f-row");
-        let elem = document.getElementsByClassName("kol");
-        if (filter.classList.contains("show")) {
-            $(this).html("Toon filters");
-            $("#f-row").removeClass("show");
-            for (i = 0; i < elem.length; i++) {
-                elem[i].classList.remove("col-3");
+        let filter = document.getElementById('f-row');
+        let elem = document.getElementsByClassName('kol');
+        if (filter.classList.contains('show')) {
+            $(this).html('Toon filters');
+            $('#f-row').removeClass('show');
+            for (let i = 0; i < elem.length; i++) {
+                elem[i].classList.remove('col-3');
             }
-            for (i = 0; i < elem.length; i++) {
-                elem[i].classList.add("col-4");
+            for (let i = 0; i < elem.length; i++) {
+                elem[i].classList.add('col-4');
             }
         } else {
-            $(this).html("Verberg filters");
-            $("#f-row").addClass("show");
-            for (i = 0; i < elem.length; i++) {
-                elem[i].classList.add("col-3");
+            $(this).html('Verberg filters');
+            $("#f-row").addClass('show');
+            for (let i = 0; i < elem.length; i++) {
+                elem[i].classList.add('col-3');
             }
-            for (i = 0; i < elem.length; i++) {
-                elem[i].classList.remove("col-4");
+            for (let i = 0; i < elem.length; i++) {
+                elem[i].classList.remove('col-4');
             }
         }
     });
