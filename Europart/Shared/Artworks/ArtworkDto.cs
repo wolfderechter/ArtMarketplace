4db7bca3491651b43e5d5be0e4ea13de4a6c602d
Zowie <zowie.verschuere@student.hogent.be>
2021-10-30 16:46:34 +0200

    Toevoegen form en validation voor create artwork

--- Europart/Shared/Artworks/ArtworkDto.cs
+++ Europart/Shared/Artworks/ArtworkDto.cs
@@ -1,9 +1,5 @@
 ﻿using EuropArt.Shared.Artist;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace EuropArt.Shared.Artworks
 {
@@ -11,10 +7,16 @@ namespace EuropArt.Shared.Artworks
     {
         public class Index
         {
+            // [Required]
             public int Id { get; set; }
             public ArtistDto.Detail Artist { get; set; }
+            [Required]
+            [StringLength(50, ErrorMessage = "Name is too long.")]
             public string Name { get; set; }
+            [Required]
+            [Range(0, 10000000, ErrorMessage = "Price cannot be negative")]
             public decimal Price { get; set; }
+            [Required]
             public string ImagePath { get; set; }
         }
 
