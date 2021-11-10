021ade258719e4dbc2bed70a81d85d1295c87d08
Zowie <zowie.verschuere@student.hogent.be>
2021-11-10 15:55:24 +0100

    Fluent validation + meertaligheid

--- Europart/Shared/Artworks/ArtworkDto.cs
+++ Europart/Shared/Artworks/ArtworkDto.cs
@@ -3,20 +3,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EuropArt.Shared.Artworks
 {
-    public static class ArtworkDto
+    public class ArtworkDto
     {
         public class Index
         {
-            // [Required]
+         
             public int Id { get; set; }
             public ArtistDto.Detail Artist { get; set; }
-            [Required]
-            [StringLength(50, ErrorMessage = "Name is too long.")]
+          
             public string Name { get; set; }
-            [Required]
-            [Range(0, 10000000, ErrorMessage = "Price cannot be negative")]
+          
             public decimal Price { get; set; }
-            [Required]
+          
             public string ImagePath { get; set; }
         }
 
---
+++ Europart/Shared/Artworks/ArtworkValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+
+namespace EuropArt.Shared.Artworks
+{
+    public class ArtworkValidator : AbstractValidator<ArtworkDto.Detail>
+    {
+        public ArtworkValidator(IStringLocalizer<Resources.Artworks.Validation> Loc)
+        {
+            RuleFor(p => p.Name).NotEmpty().WithMessage(Loc["Title"]);
+            RuleFor(p => p.Name).MaximumLength(100).WithMessage(Loc["BuyNow"]);
+            RuleFor(p => p.Price).NotEmpty().WithMessage(Loc["Price"]);
+                
+        }
+    }
+}
