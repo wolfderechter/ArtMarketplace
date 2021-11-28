c15896cd2779bba1644ebc99248342695ed383eb
Stef Boerjan <stef.boerjan@student.hogent.be>
2021-11-29 00:57:27 +0100

    [add] account button in navbar and temp account page

--- Europart/Shared/Resources/Account.Designer.cs
+++ Europart/Shared/Resources/Account.Designer.cs
@@ -22,14 +22,14 @@ namespace EuropArt.Shared.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Login {
+    public class Account {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Login() {
+        internal Account() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace EuropArt.Shared.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EuropArt.Shared.Resources.Login", typeof(Login).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EuropArt.Shared.Resources.Account", typeof(Account).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
